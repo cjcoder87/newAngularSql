ab8447bd0e865b28a0e72d1d0016f7fb882cfbf8
Christopher Jackson (Infosys BPM Limited) <v-chrij@microsoft.com>
2019-01-17 18:14:23 -0800

    changed row highlight

--- ClientApp/src/app/rowcolor.js
+++ ClientApp/src/app/rowcolor.js
@@ -1,7 +1,7 @@
 function rowHighlight(x) {
     if (x.checked){
         var tr = x.closest('table tbody tr');
-    tr.style.backgroundColor = 'rgba(71, 186, 231, 0.575)';
+    tr.style.backgroundColor = 'rgba(71, 186, 231, 0.12)';
     }
     else{
         var tr = x.closest('table tbody tr');
